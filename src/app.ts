071b9f62614216f81f23be932623e61feb15337f
Nathan <nathan.ar.94@gmail.com>
2025-02-07 00:42:46 -0300

    fix: app

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ app.use((req: Request, res: Response, next: NextFunction) => {
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization"); 
 
   if (req.method === "OPTIONS") {
-    return res.sendStatus(204); // Responde rapidamente às requisições OPTIONS
+    return res.sendStatus(204);
   }
 
   next();
