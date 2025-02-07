b1c3a925429e263f39dcc962b0bdc1b895176a96
Nathan <nathan.ar.94@gmail.com>
2025-02-06 21:11:06 -0300

    feat: cors habilitado

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,23 @@
 import "reflect-metadata";
 import express from "express";
+import { Request, Response, NextFunction } from "express";
 import { createConnection } from "typeorm";
 import passport from "passport";
 
 const app = express();
 app.use(express.json());
 app.use(passport.initialize());
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.header("Access-Control-Allow-Origin", "*"); 
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); 
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization"); 
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204); // Responde rapidamente às requisições OPTIONS
+  }
+
+  next();
+});
 const PORT = 3000;
 
 import { initializerRoutes } from "./api/routers";
